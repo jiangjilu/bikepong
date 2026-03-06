df434bdd05401ffdcaf16b2c901584f08833dda2
Bill <185852471@qq.com>
2026-03-07 07:33:41 +0800

    ProRes 4444 Format

--- remotion.config.ts
+++ remotion.config.ts
@@ -6,8 +6,15 @@
 import { Config } from "@remotion/cli/config";
 import { enableTailwind } from "@remotion/tailwind-v4";
 
+// 透明视频配置 - ProRes 格式（适合视频剪辑软件）
 Config.setVideoImageFormat("png");
-Config.setPixelFormat("yuva420p");
-Config.setCodec("vp9");
+Config.setPixelFormat("yuva444p10le");
+Config.setCodec("prores");
+Config.setProResProfile("4444");
 Config.setOverwriteOutput(true);
 Config.overrideWebpackConfig(enableTailwind);
+
+// 如果需要 WebM 格式（适合浏览器），使用以下配置：
+// Config.setVideoImageFormat("png");
+// Config.setPixelFormat("yuva420p");
+// Config.setCodec("vp9");
